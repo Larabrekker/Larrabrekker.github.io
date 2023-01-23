3e1580f4e1b606157d924f545016cc1eef948d27
Larabrekker <108152020+Larabrekker@users.noreply.github.com>
2023-01-23 23:07:31 +0000

    hi

--- chat.js
+++ chat.js
@@ -20,8 +20,3 @@ $(function() {
 
 
 
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://fewcywtpditheaemdsbb.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
